d7fc122c36a2d5120acb8fdc6b0f6d4eb34f34e5
ljhxu <277841643@qq.com>
2022-08-08 23:55:22 +0800

    feat: 'reactive 触发依赖 与 收集依赖的 effect'

--- src/reactivity/test/effect.spec.ts
+++ src/reactivity/test/effect.spec.ts
@@ -1,5 +1,23 @@
+
+import {reactive} from '../reactive'
+
+import {effect} from '../effect'
 describe('effect', ()=>{
-    it('happy path', ()=>{
+    it.skip('happy path', ()=>{
+        const user = reactive({
+            age:10
+        })
+        let nextAge
+
+        effect(()=>{
+            nextAge = user.age + 1
+        })
+        expect(nextAge).toBe(11)
         
+
+        // update
+
+        user.age++ 
+        expect(nextAge).toBe(12)
     })
 })
